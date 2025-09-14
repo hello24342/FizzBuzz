3da86544026b302d54f763e93d772f607f6f97ad
hello <hellojennifer.du@mail.utoronto.ca>
2025-09-13 21:50:39 -0400

    MULTIPLES PLS WORK THIS TIME

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main();
     }
 
 }
--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,22 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+
+        multiples();
+    }
+
+    private static void multiples() {
+        int total = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            // Print our appropriate result.
+            if (divisibleBy3 || divisibleBy5) {
+                total++;
+
+            }
+        }
+        System.out.println(total);
+    }
 }
